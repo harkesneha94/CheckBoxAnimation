e1ebd3d8d93ab5ccfe620c53b0673f1df523cd01
Sneha Harke <Sneha.Harke@ril.com>
2019-08-09 13:58:55 +0530

    Initial Commit

---
+++ CheckBoxAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckBoxAnimation
+//
+//  Created by Sneha Harke on 09/08/19.
+//  Copyright © 2019 Sneha Harke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
